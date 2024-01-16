ec7854574a42a42b4c37e1d73f4a4b92d47ce33a
JaskirtKaler <96710929+JaskirtKaler@users.noreply.github.com>
2024-01-16 00:54:50 -0800

    SVG
    what the hell are SVG's

--- personal-portfolio/src/components/Circle.js
+++ personal-portfolio/src/components/Circle.js
@@ -4,22 +4,36 @@ import './Circle.css';
 function Circle() {
   return (
     <div className='skill'>
-        <div className='outer'>
-            <div className='inner'>
-                <div id='number'>
-                    65%
-                </div>
-            </div>
-        </div>
-        <svg className='circle-svg' xmlns="http://www.w3.org/2000/svg" version="1.1" width="160px" height="160px">
-            <defs>
-                <linearGradient id="GradientColor">
-                <stop offset="0%" stop-color="#e91e63" />
-                <stop offset="100%" stop-color="#673ab7" />
-                </linearGradient>
-            </defs>
-            <circle className='svg-circle' cx="80" cy="80" r="70" stroke-linecap="round" />
-        </svg> 
+            <svg className='circle-svg' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 36 36">
+                <defs>
+                    <linearGradient id="GradientColor">
+                        <stop offset="0%" stop-color="#e91e63" />
+                        <stop offset="100%" stop-color="#673ab7" />
+                    </linearGradient>
+                    <filter id="shadow" x="-20%" y="-20%" width="140%" height="140%">
+                        <feGaussianBlur in="SourceAlpha" stdDeviation="3"/>
+                        <feOffset dx="6" dy="6" result="offsetblur"/>
+                        <feComponentTransfer>
+                            <feFuncA type="linear" slope="0.5"/>
+                        </feComponentTransfer>
+                        <feMerge> 
+                            <feMergeNode/>
+                            <feMergeNode in="SourceGraphic"/> 
+                        </feMerge>
+                    </filter>
+                </defs>
+                <path className="circle"
+                    d="M18 2.0845
+                        a 15.9155 15.9155 0 0 1 0 31.831
+                        a 15.9155 15.9155 0 0 1 0 -31.831"
+                    fill="none"
+                    stroke="url(#GradientColor)"
+                    stroke-width="4"
+                    stroke-dasharray="65, 100"
+                    filter="url(#shadow)" />
+                    <text x="50%" y="50%" text-anchor="middle" fill="#FFF" dy=".3em" font-size='10px'>65%</text>
+            </svg>
+        
     </div>
   )
 }
